2f7b0b45a586b474aabee24f327855c813df2f03
MUBIRI <mubirirobberts@gmail.com>
2023-04-07 10:32:56 +0000

    Debug fix

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Task 0"""
+import asyncio
+import Generator from typing
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    The coroutine will loop 10 times
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
